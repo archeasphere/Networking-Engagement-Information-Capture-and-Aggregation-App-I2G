ae2cbc3366f105deb435a3b6ba1c59c1cabc0d49
Flennon21 <95154276+Flennon21@users.noreply.github.com>
2025-03-17 00:56:43 -0700

    database upload

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+<<<<<<< HEAD
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
@@ -17,3 +18,24 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+=======
+const functions = require("firebase-functions");
+const {Pool} = require("pg");
+
+// PostgreSQL connection pool
+const pool = new Pool({
+  connectionString: "postgres://youruser:yourpassword@your-remote-host:5432/yourdatabase",
+  ssl: {rejectUnauthorized: false},
+});
+
+// Firebase Function to Fetch Data from PostgreSQL
+exports.getFiles = functions.https.onRequest(async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM files");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Database Error:", error);
+    res.status(500).json({error: "Internal Server Error"});
+  }
+});
+>>>>>>> d3759f0 (Initial commit - Added Firebase functions and config files)
