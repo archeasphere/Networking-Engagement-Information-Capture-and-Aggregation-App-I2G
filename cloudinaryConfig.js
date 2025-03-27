b1da09096ee88dfc1c263c146b71454e277a1e67
Flennon21 <95154276+Flennon21@users.noreply.github.com>
2025-03-26 17:15:38 -0700

    Debugging cloudinary config

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
-// Configure Cloudinary
+// Debug
+console.log("Cloudinary Config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "✔️ Exists" : "❌ Missing"
+});
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
