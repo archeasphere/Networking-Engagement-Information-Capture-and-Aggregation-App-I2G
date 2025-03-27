e03718a9b268c61bc04724688798f0eacefb2d39
Flennon21 <95154276+Flennon21@users.noreply.github.com>
2025-03-26 17:23:54 -0700

    Debugging cloudinary config

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -3,15 +3,15 @@ const cloudinary = require("cloudinary").v2;
 
 // Debug
 console.log("Cloudinary Config:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "✔️ Exists" : "❌ Missing"
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET ? "✔️ Exists" : "❌ Missing"
 });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
